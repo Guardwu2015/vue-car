3be03d5ecc71ece2095626d5ea96f0be46764933
武岳 <wuyue@wuyuedeMacBook-Pro.local>
2019-05-30 23:30:29 +0800

    split ui

---
+++ src/components/dynamicContent.vue
@@ -0,0 +1,131 @@
+<template>
+  <aside id="dynamicContent">
+      <!--////////// 00. Background //////////-->
+      <div id="cardBack">
+      </div>
+      <!--////////// 01. Dimensions //////////-->
+      <div id="content-dimensions">
+          <h1>Dimensions</h1>
+          <p class="copy">FF 91's 126-inch long wheelbase and extended platform is a new benchmark in comfort and refinement. The welcome
+              absence of a gas powertrain allowed us to create a unique interior architecture that maximizes space and
+              luxury.</p>
+      </div>
+
+      <!--////////// 02. Battery //////////-->
+      <div id="content-battery">
+          <h1>Battery</h1>
+          <p class="copy">Our 130 kWh lithium-ion battery pack achieves the world’s highest energy density of any automotive battery currently
+              in production. This enables a tremendous increase in range, well in excess of 300 miles on a single charge
+              and 200 miles on a 30-minute fast charge.</p>
+      </div>
+
+      <!--////////// 03. Powertrain //////////-->
+      <div id="content-powertrain">
+          <svg id="powertrainSlider" class="svgSlider">
+              <polyline class="lineArrow" points="70 22 50 2 30 22" />
+              <polyline class="lineArrow" points="30 478 50 498 70 478" />
+              <line class="lineDashed" x1="50" y1="50" x2="50" y2="450" />
+              <g id="powerKnob" class="knob">
+                  <circle class="circle" cx="50" cy="250" r="50" />
+                  <polyline class="lineDiamond" points="30 250 50 270 70 250 50 230 30 250 70 250" />
+              </g>
+          </svg>
+          <div id="powertrainText">
+              <h1>Powertrain</h1>
+              <ul>
+                  <li>3 Electric Motors</li>
+                  <li>1,050 Horsepower</li>
+                  <li>0 - 60 mph in 2.39 seconds</li>
+                  <li>Dynamic Vehicle Control</li>
+              </ul>
+          </div>
+      </div>
+
+      <!--////////// 04. 4-Wheel Steering //////////-->
+      <div id="content-steering">
+          <h1>4-Wheel Steering</h1>
+          <p class="copy">This feature allows all four wheels to act in unison to give FF 91 incredibly responsive and nimble handling,
+              comparable with the turning radius of a much smaller vehicle.</p>
+
+          <svg id="steeringSlider" class="svgSlider">
+              <polyline class="lineArrow" points="22 70 2 50 22 30" />
+              <polyline class="lineArrow" points="478 30 498 50 478 70" />
+              <line class="lineDashed" x1="50" y1="50" x2="450" y2="50" />
+              <g id="steerKnob" class="knob">
+                  <circle class="circle" cx="250" cy="50" r="50" />
+                  <polyline class="lineDiamond" points="250 30 270 50 250 70 230 50 250 30 250 70" />
+              </g>
+          </svg>
+      </div>
+
+      <!--////////// 05. Front Lighting //////////-->
+      <div id="content-front-lighting">
+          <h1>Front Lighting</h1>
+          <div class="frontLightBut active">
+              <svg>
+                  <polyline class="edge" points="20,2 38,20 20,38 2,20 20,2" />
+                  <polyline class="inner" points="20,12 28,20 20,28 12,20 20,12" />
+              </svg>
+              <p>Daytime running lights</p>
+          </div>
+          <div class="frontLightBut active">
+              <svg>
+                  <polyline class="edge" points="20,2 38,20 20,38 2,20 20,2" />
+                  <polyline class="inner" points="20,12 28,20 20,28 12,20 20,12" />
+              </svg>
+              <p>Low beams</p>
+          </div>
+          <div class="frontLightBut">
+              <svg>
+                  <polyline class="edge" points="20,2 38,20 20,38 2,20 20,2" />
+                  <polyline class="inner" points="20,12 28,20 20,28 12,20 20,12" />
+              </svg>
+              <p>High beams</p>
+          </div>
+          <div class="frontLightBut">
+              <svg>
+                  <polyline class="edge" points="20,2 38,20 20,38 2,20 20,2" />
+                  <polyline class="inner" points="20,12 28,20 20,28 12,20 20,12" />
+              </svg>
+              <p>Fog lamps</p>
+          </div>
+      </div>
+
+      <!--////////// 06. Rear Lighting //////////-->
+      <div id="content-rear-lighting">
+          <h1>Rear Lighting</h1>
+          <svg id="rearSlider" class="svgSlider">
+              <polyline class="lineArrow" points="478 30 498 50 478 70" />
+              <polyline class="lineArrow" points="22 70 2 50 22 30" />
+              <polyline class="lineArrow" points="270 128 250 148 230 128" />
+              <line class="lineDashed" x1="50" y1="50" x2="450" y2="50" />
+              <g id="rearKnob" class="knob">
+                  <circle class="circle" cx="250" cy="50" r="50" />
+                  <polyline class="lineDiamond" points="250 30 270 50 250 70 230 50 250 30 250 70" />
+              </g>
+          </svg>
+      </div>
+
+      <!--////////// 07. Aerodymanics //////////-->
+      <div id="content-aerodynamics">
+          <h1>Aerodynamics</h1>
+          <p class="copy">Most luxury vehicles of this size and shape have a drag coefficient in excess of 0.3. Our target is to get it
+              below 0.26. At highway speeds, this 13% reduction in aerodynamic drag gives you a 5% improvement in range.</p>
+      </div>
+
+      <svg width="90" height="90" id="closeX">
+          <path class="tan" d="M29,5.1c-5.2,2.1-10.2,5.3-14.4,9.5c-16.8,16.8-16.8,44,0,60.8c4.2,4.2,9.2,7.4,14.4,9.5" />
+          <path class="tan" d="M61,84.9c5.2-2.1,10.2-5.3,14.4-9.5c16.8-16.8,16.8-44,0-60.8c-4.2-4.2-9.2-7.4-14.4-9.5" />
+          <line class="gray" x1="32" y1="58" x2="58" y2="32" />
+          <line class="gray" x1="32" y1="32" x2="42" y2="42" />
+          <line class="gray" x1="48" y1="48" x2="58" y2="58" />
+      </svg>
+  </aside>
+</template>
+<script>
+export default {
+  name: 'dynamicContent'
+}
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ src/components/mNav.vue
@@ -0,0 +1,151 @@
+<template>
+  <nav id="navBox">
+      <div id="navInner">
+          <div class="title">FF 91</div>
+          <ol class="navList">
+              <li class="navItem">Dimensions</li>
+              <li class="navItem">Battery</li>
+              <li class="navItem">Powertrain</li>
+              <li class="navItem">4-Wheel Steering</li>
+              <li class="navItem">Front Lighting</li>
+              <li class="navItem">Rear Lighting</li>
+              <li class="navItem">Aerodynamics</li>
+              <li class="navItem">Free Viewing</li>
+          </ol>
+          <span class="bigBut" id="reserveBut" style="display: none">Reserve</span>
+          <br/>
+          <span class="bigBut" id="subscribeBut" style="display: none">Subscribe</span>
+          <div id="socialBox" style="display: none">
+              <svg class="social" id="Twitter" viewBox="0 0 112 112">
+                  <path d="M47,76c18,0,28-15,28-28h0l5-5-6,2a10,10,0,0,0,4-5l-6,2a10,10,0,0,0-17,7v2A28,28,0,0,1,35,38a10,10,0,0,0,3,13H34a10,10,0,0,0,8,10H37a10,10,0,0,0,9,7,20,20,0,0,1-12,4H32a28,28,0,0,0,15,4"
+                  />
+              </svg>
+              <svg class="social" id="Facebook" viewBox="0 0 112 112">
+                  <path d="M77,32H35a3,3,0,0,0-3,3V77a3,3,0,0,0,3,3H58V61H51V54h6V49c0-6,4-10,9-10h6v6H69c-3,0-4,1-4,4v5h7v7H65V80H77a3,3,0,0,0,3-3h0V35A3,3,0,0,0,77,32Z"
+                  />
+              </svg>
+              <svg class="social" id="LinkedIn" viewBox="0 0 112 112">
+                  <path d="M76,32H36a4,4,0,0,0-4,3V77a4,4,0,0,0,4,3H76a4,4,0,0,0,4-3V35A4,4,0,0,0,76,32ZM46,73H39V50h7ZM43,47a4,4,0,1,1,4-4A4,4,0,0,1,43,47ZM73,73H66V62c0-3,0-6-4-6s-4,3-4,6V73H51V50h7v3h0a7,7,0,0,1,6-4c7,0,9,5,9,11Z"
+                  />
+              </svg>
+          </div>
+      </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'mNav'
+}
+</script>
+
+<style lang="scss" scoped>
+  nav {
+    pointer-events: none;
+    user-select: none;
+    -ms-user-select: none;
+    -moz-user-select: none;
+    font-size: 14px;
+    z-index: 1;
+  }
+  #navBox {
+    #navInner {
+      position: absolute;
+      right: 20px;
+      top: 50%;
+      margin-top: -333px;
+      text-align: right;
+      display: none;
+      opacity: 0;
+      transition: opacity 1s ease-in-out, color 0.3s ease-in-out;
+      touch-action: none;
+      .title {
+        font-size: 5em;
+        font-weight: 300;
+        text-align: right;
+        margin: 0 -15px 15px 0;
+        line-height: 1em;
+      }
+      &.inverted {
+        color: #cccccc;
+        .social {
+          fill: #cccccc;
+        }
+      }
+
+      .navList {
+        margin: 0;
+        padding: 0;
+        list-style-type: none;
+        counter-reset: elementcounter;
+        font-weight: 400;
+      }
+      .navItem {
+        pointer-events: auto;
+        text-align: right;
+        padding-bottom: 30px;
+        cursor: pointer;
+        transform-origin: 100% 50%;
+        touch-action: none;
+        &::after {
+          content: "0"counter(elementcounter);
+          counter-increment: elementcounter;
+          font-size: 0.7em;
+          width: 50px;
+          border-bottom: 2px solid #808080;
+          display: inline-block;
+          margin-left: 10px;
+          transition: all 0.3s ease;
+        }
+        &.active {
+          color: #9a614c;
+          &::after {
+            width: 70px;
+            padding-right: 10px;
+          }
+        }
+        &:hover {
+          &::after {
+            width: 60px;
+          }
+        }
+      }
+      .bigBut {
+        color: #fff;
+        background: #9a614c;
+        font-weight: 700;
+        text-align: center;
+        text-transform: uppercase;
+        display: inline-block;
+        padding: 12px 0;
+        margin: 0px auto 12px;
+        width: 175px;
+        border: 1px solid #9a614c;
+        cursor: pointer;
+        pointer-events: auto;
+        transition: all 0.5s ease-in-out;
+        &:hover {
+          background: transparent;
+          color: #9a614c;
+        }
+        &::after {
+          font-size: 1.3em;
+          line-height: 0.5em;
+          content: " \2192";
+        }
+      }
+      .social {
+        width: 56px;
+        height: 56px;
+        fill: #333333;
+        position: relative;
+        cursor: pointer;
+        pointer-events: auto;
+        transition: 0.3s fill ease-in-out;
+        &:hover {
+          fill: #9a614c;
+        }
+      }
+    }
+  }
+</style>
